6f29c20026f3625381cc4adc0220550c557a7999
carlosjac312 <115487243+carlosjac312@users.noreply.github.com>
2023-05-08 19:09:17 +0200

    Delete Ing_Soft/Interfaces ingSoft/InterfacesNftrader directory

--- Ing_Soft/Interfaces ingSoft/InterfacesNftrader/src/Launch.java
+++
@@ -1,9 +0,0 @@
-public class Launch {
-    public static void main(String[] args) {
-        //Buscador a=new Buscador();
-        //PerfilNFT b=new PerfilNFT();
-        //ChatsInterface c=new ChatsInterface();
-        //perfilUser d=new perfilUser();
-        CatalogoInterface e=new CatalogoInterface();
-    }
-}
